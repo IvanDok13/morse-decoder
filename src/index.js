682fc58975fd5816beb7abe9222ea0a0e2034ac6
Ivan Klochkov <vany-nov13@yandex.ru>
2023-08-28 11:35:51 +0300

    fix: change solution

--- src/index.js
+++ src/index.js
@@ -40,13 +40,12 @@ const MORSE_TABLE = {
 function decode(expr) {
   let morsStr = '';
   let morsSymb = [];
-  for (let i = 0; i < expr.length - 1; i += 10) {
-    morsSymb.push(expr.slice(i + 2, i + 10));
+  for (let i = 0; i < expr.length; i += 10) {
+    morsSymb.push(expr.slice(i, i + 10));
   }
-  
   for (let j = 0; j < morsSymb.length; j++) {
     for (let k = 1; k < morsSymb[j].length; k += 2) {
-      if (morsSymb[j][k] === '*' && morsSymb[j][k+6] === '*') morsStr += ' ';
+      if (morsSymb[j][k] === '*' && morsSymb[j][k+8] === '*') morsStr += ' ';
       if (morsSymb[j][k] === '1') morsStr += '-';
       if (morsSymb[j][k] === '0' && morsSymb[j][k - 1] === '1') morsStr += '.';
     }
